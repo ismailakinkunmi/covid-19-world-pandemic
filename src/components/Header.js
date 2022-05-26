2f8982f087009877693d21fcc2bf06dee516fd2e
Adekunle Ismail <adismail4@gmail.com>
2022-05-26 10:22:43 +0100

    Add css files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import image from '../assests/new.jpg';
-import styles from './styles/Header.module.css';
+import React from "react";
+import image from "../assests/new.jpg";
+import styles from "./styles/Header.module.css";
 
 const Header = () => (
   <>
     <div className={styles.image}>
       <img src={image} alt="europe" />
       <h1 className={styles.text}>
-        Global Covid 19 Cases
-        {' '}
+        Daily Cases
         <br />
         Updated Reports
       </h1>
