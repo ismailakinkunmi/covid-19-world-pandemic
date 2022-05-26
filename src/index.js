72ab05b3a98b34c254b9eaaaa9c4259821ff174f
Adekunle Ismail <adismail4@gmail.com>
2022-05-26 16:12:40 +0100

    Add client to react-dom in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import store from './redux/configureStore';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import store from "./redux/configureStore";
 
-ReactDOM.render(
-  <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-    ,
-  </BrowserRouter>,
-
-  document.getElementById('root'),
+  </Router>
 );
